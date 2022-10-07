a632ca24a5530aac3bea5d3df26d3c6f8482c549
clearho <clearan77@gmail.com>
2022-10-07 16:21:06 +0800

    添加icon，使用不同排序

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,23 @@
 <template>
-  <button class="g-button"><slot></slot></button>
+  <button class="g-button" :class=`icon-${iconPosition}`>
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <span class="content">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script>
 export default {
-
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+    }
+  }
 }
 </script>
 
@@ -16,14 +29,43 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+
   &:hover {
+    cursor: pointer;
     border-color: var(--border-color-hover)
   }
+
   &:active {
     background-color: var(--button-active-bg)
   }
+
   &:focus {
     outline: none;
+  }
+
+  .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+
+  .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    .icon {
+      order: 2;
+      margin-left: .3em;
+      margin-right: 0;
+    }
+
+    .content {
+      order: 1;
+    }
   }
 }
 
