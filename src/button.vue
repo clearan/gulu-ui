ba611896c2eabf42f9a76d0103cc455ec4ef2597
clearho <clearan77@gmail.com>
2022-10-07 23:27:44 +0800

    添加菊花loading状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class=`icon-${iconPosition}`>
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <span class="content">
       <slot></slot>
     </span>
@@ -23,6 +24,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -67,6 +77,9 @@ export default {
     .content {
       order: 1;
     }
+  }
+  .loading {
+    animation: spin 2s infinite linear;
   }
 }
 
