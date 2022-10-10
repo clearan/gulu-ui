a51a5d2412fb58cdf9e5192ac4db3520bad7d10e
clearho <clearan77@gmail.com>
2022-10-10 23:10:46 +0800

    添加loading状态的click事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="g-button" :class=`icon-${iconPosition}`>
-    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-    <g-icon class="loading" name="loading"></g-icon>
+  <button class="g-button" :class=`icon-${iconPosition}` @click="$emit('click')">
+    <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon v-if="loading" class="loading icon" name="loading"></g-icon>
     <span class="content">
       <slot></slot>
     </span>
@@ -18,8 +18,12 @@ export default {
       validator(value) {
         return value !== 'left' || value !== 'right';
       }
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
-  }
+  },
 }
 </script>
 
